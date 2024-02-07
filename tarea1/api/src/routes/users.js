0275c6a1ae317eb7da250462941766599d926584
Isaac <isaac.noriega@iteso.mx>
2024-02-07 12:26:37 -0600

    Tarea 1

---
+++ tarea1/api/src/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+//Rutas de usuario
+router.get('/users', userController.getUsers);
+router.get('/user/:id',userController.getUserById);
+router.post('/user',userController.createUser);
+router.put('/user/:id',userController.updateUser);
+router.delete('/user/:id',userController.deleteUser);
+module.exports= router;
